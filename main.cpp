06b86c1d576034de280d0dd9a1ba2e7f948a9f58
福百川 刘 <brick5322@outlook.com>
2023-08-08 23:46:05 +0800

    修改了test的文件存放位置

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include "Networking/DHCP/Message.h"
 #include <stdio.h>
+
 using namespace bric::Networking;
 
 int main()
 {
-    FILE *fp = fopen("discover.bin","rb");
+    FILE *fp = fopen("resources/discover.bin","rb");
     uint8_t buffer[350];
     size_t size = fread(buffer,300,1,fp);
 
